4052eb2310fb0782da89457a5ef05b66adcff983
Eduarda Matos <111284160+dudamdev@users.noreply.github.com>
2023-03-07 22:05:22 -0300

    teste

---
+++ teste/src/fundamentos/ExercicioDois.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fundamentos;
+
+/**
+ *
+ * @author 1091392223010
+ */
+public class ExercicioDois {
+    public static void main(String[] args) {
+        System.out.println("1, 2, 3, 4");
+        System.out.print("1, ");
+        System.out.print(2 + ", ");
+        System.out.print("3, ");
+        System.out.print(4 + " \n");
+        System.out.printf("%d, %d, %d, %d%n", 1, 2, 3, 4);
+        
+    }
+}
