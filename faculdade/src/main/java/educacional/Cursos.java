bf2e9266ae04085b879be2ffae6d5f0b2e7732aa
Eduarda Matos <111284160+dudamdev@users.noreply.github.com>
2023-03-07 22:18:51 -0300

    Aula 07.03

---
+++ faculdade/src/main/java/educacional/Cursos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package educacional;
+
+/**
+ *
+ * @author 1091392223010
+ */
+public class Cursos {
+    
+}
---
+++ faculdade/src/main/java/pessoa/Aluno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pessoa;
+
+/**
+ *
+ * @author 1091392223010
+ */
+public class Aluno {
+    
+}
