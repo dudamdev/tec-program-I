d4a5b0504af1ff276fece0eb04ec74fa158dffde
Eduarda Matos <duda.mdev@gmail.com>
2023-03-27 10:40:14 -0300

    Fix ExTwo

--- exercicios1/src/main/java/exercicios/ExTwo.java
+++ exercicios1/src/main/java/exercicios/ExTwo.java
@@ -4,42 +4,37 @@
  */
 package exercicios;
 import java.util.Scanner;
+import java.util.*;
 
 /**
  *
  * @author 1091392223010
  */
+
 public class ExTwo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num, cont;
-        boolean primo;
         
         System.out.print("Digite um número: ");
         num = input.nextInt();
         
-        for(int i = 2; i < num; i++) {
-            if (num != 1 && num != 2) {
-                for (int j = 1; j < num; j++) {
-                    
-                }
-                if (num % i == 0) {
+        int[] vetorPrimo = new int[num];
+        int indice = 0;
+        for (int i = 2; i <= num; i++) {
+            boolean primo = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
                     primo = false;
+                    break;
                 }
-                if (num % i != 0) {
-                    primo = true;
-                }
-            }else {
-                primo = true;
             }
-            
             if (primo) {
-                System.out.println("O número é primo");
-            }else {
-                System.out.println("O número não é primo");
-1, 2, 3, 5
+                vetorPrimo[indice] = i;
+                indice++;
             }
         }
-        
+        System.out.println("Números primos menores ou iguais a " + num + ": " + Arrays.toString(Arrays.copyOf(vetorPrimo, indice)));
     }
 }
+
