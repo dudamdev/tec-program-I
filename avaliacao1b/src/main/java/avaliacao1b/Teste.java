4f570f93f90c5b35fb71ba5daf3f41bea340379a
Eduarda Matos <duda.mdev@gmail.com>
2023-04-13 22:03:38 -0300

    Correção da prova

--- avaliacao1b/src/main/java/avaliacao1b/Teste.java
+++ avaliacao1b/src/main/java/avaliacao1b/Teste.java
@@ -15,11 +15,32 @@ public class Teste {
         double metroCubico = 2.5;
         double barril = 3.0;
                
-        System.out.println("2.8 quilometros correspondem a " + ConversaoDeDados.kmEmMetros(quilometro) + " metros");
-        System.out.println("2.5 litros correspondem a " + ConversaoDeDados.litrosEmDecilitros(litro)+ " decilitros");
-        System.out.println("2.5 metros cúbicos correspondem a " + ConversaoDeDados.metroCubicosEmPesCubicos(metroCubico) + " pés cúbicos");
-        System.out.println("3.0 barris correspondem a " + ConversaoDeDados.barrilEmDecalitros(barril) + " decalitros");
-        System.out.println("3.0 barris correspondem a " + ConversaoDeDados.barrilEmLitros(barril) + " litros");
+        System.out.println(quilometro + " quilometros correspondem a " + 
+                ConversaoDeDados.kmEmMetros(quilometro) + " metros");
+        System.out.println(litro + " litros correspondem a " + 
+                ConversaoDeDados.litrosEmDecilitros(litro)+ " decilitros");
+        System.out.println(metroCubico + " metros cúbicos correspondem a " + 
+                ConversaoDeDados.metroCubicosEmPesCubicos(metroCubico) + " pés cúbicos");
+        System.out.println(barril + " barris correspondem a " + 
+                ConversaoDeDados.barrilEmDecalitros(barril) + " decalitros");
+        System.out.println(barril + " barris correspondem a " + 
+                ConversaoDeDados.barrilEmLitros(barril) + " litros");
+        
+        System.out.println();
+        System.out.println("Strings formatadas com %f:");
+        System.out.println();
+
+//      Strings formatadas
+        System.out.printf("%.2f quilometros correspondem a %.2f metros %n",
+                quilometro, ConversaoDeDados.kmEmMetros(quilometro));
+        System.out.printf("%.2f litros correspondem a %.2f decilitros %n", 
+                litro,ConversaoDeDados.litrosEmDecilitros(litro));
+        System.out.printf("%.2f metros cúbicos correspondem a %.2f metros cúbicos %n", 
+                metroCubico, ConversaoDeDados.metroCubicosEmPesCubicos(metroCubico));
+        System.out.printf("%.2f barris correspondem a %.2f barris %n", 
+                barril, ConversaoDeDados.barrilEmDecalitros(barril));
+        System.out.printf("%.2f barris correspondem a %.2f barris %n", 
+                barril, ConversaoDeDados.barrilEmLitros(barril));
       
     }
 }
