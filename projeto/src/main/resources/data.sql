b3a5de61e59c958316f78218d5196f8fcdd0ad86
dudamdev <duda.mdev@gmail.com>
2023-05-23 20:53:10 -0300

    Delete data.sql

--- projeto/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO CONTATO (name, email) VALUES ('Alexandre', 'alexandre@fatec.com.br'); 
-INSERT INTO CONTATO (name, email) VALUES ('José', 'jose@fatec.com.br');
-INSERT INTO CONTATO (name, email) VALUES ('João', 'joao@fatec.com.br');
-INSERT INTO CONTATO (name, email) VALUES ('Maria', 'maria@fatec.com.br');
-INSERT INTO CONTATO (name, email) VALUES ('Joaquina', 'joaquina@fatec.com.br');
