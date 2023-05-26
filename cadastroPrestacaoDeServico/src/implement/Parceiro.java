c0a893b83deea0a53327e026808bca0990d5175b
dudamdev <duda.mdev@gmail.com>
2023-05-25 21:58:31 -0300

    Aula 25/05

---
+++ cadastroPrestacaoDeServico/src/implement/Parceiro.java
@@ -0,0 +1,34 @@
+package implement;
+
+public class Parceiro extends Juridica {
+    private String tipoPessoa;
+    private Double desempenho;
+
+    public String getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(String tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public Double getDesempenho() {
+        return desempenho;
+    }
+
+    public void setDesempenho(Double desempenho) {
+        this.desempenho = desempenho;
+    }
+    
+    public void cadastrar() {
+
+    }
+
+    public void bloquear() {
+
+    }
+
+    public void excluir() {
+
+    }
+}
